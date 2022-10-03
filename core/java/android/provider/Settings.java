yaap/frameworks_base_old

base: Allow toggling screen off FOD [1/2]
idoybh (YAAP): Change default to enabled to retain AOSP behaviour

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

core/java/android/provider/Settings.java
@@ -5306,6 +5306,14 @@ public final class Settings {
         public static final String NOTIFICATION_HEADERS = "notification_headers";
 
         /**
+         * Enable fod detection even when screen is off
+         * Default value is 0
+         * @hide
+         */
+        @Readable
+        public static final String SCREEN_OFF_FOD = "screen_off_fod";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate

HurtCopain/derp13_fwb

Change-Id: I1feece846e4dbddb276ad6ed0efac77e5b7986d8
Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

core/java/android/provider/Settings.java
@@ -10985,6 +10985,14 @@ public final class Settings {
                 "launcher_taskbar_education_showing";
 
         /**
+         * Enable udfps detection even when screen is off
+         * Default value is 0
+         * @hide
+         */
+        @Readable
+        public static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */